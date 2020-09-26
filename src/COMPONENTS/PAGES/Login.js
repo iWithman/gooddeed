143ae1c3eb5f60121a5657bf16f6c321b77113db
rvargas_xforce <vargasr@knights.ucf.edu>
2020-09-26 15:04:21 -0400

    Rearanging of files, pages = website landing pages, helpers = helper components

---
+++ src/COMPONENTS/PAGES/Login.js
@@ -0,0 +1,10 @@
+import React,{Component} from "react";
+import "../../CSS/PAGES/Login.css"
+
+class Login extends Component{
+    render(){
+        return <h1>LOGING PAGE</h1>
+    }
+}
+
+export default Login;
